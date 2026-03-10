4dbafd421e3cc968d398a7325d128035af39b83d
Violet Quinn <159643239+vivithequeen@users.noreply.github.com>
2026-03-10 17:20:05 -0400

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -19,7 +19,6 @@ if status is-interactive # Commands to run in interactive sessions can go here
 
     # Aliases
     alias pamcan pacman
-    alias ls 'eza --icons'
     alias clear "printf '\033[2J\033[3J\033[1;1H'"
     alias q 'qs -c ii'
     
